5b723963f784ac69bbf128d846d27b54ca2f87d2
Victor Stan <victor.stan@gmail.com>
2023-02-03 09:04:10 -0500

    updated size multiplier

--- Constants.cs
+++ Constants.cs
@@ -6,7 +6,7 @@ namespace VSharpBSP
         // Some config settings referenced from here: https://github.com/radiatoryang/scopa/blob/49fbf8519fe480d02327c71b843ab483081d0ff5/Runtime/ScopaMapConfigAsset.cs
         // (default: 0.03125, 1 m = 32 units) The global scaling factor for all brush geometry and entity origins.
         public const double d_scaleMultiple = 0.03125;
-        public const float f_scaleMultiple = 0.032F;
+        public const float f_scaleMultiple = 0.03125f;
         public const float doorTriggerPadding = 2f;
         public const string entScriptDir = "Assets/Scripts/VSharpBSP/Entities";
         public const string mapAssetsPath = "Assets/baseq3/maps/";
