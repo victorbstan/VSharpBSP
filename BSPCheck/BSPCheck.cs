05fe014a691f9c34138c86e397fc672388dc4e4d
Victor Stan <victor.stan@gmail.com>
2024-02-05 20:05:47 +0100

    added BSP check placeholder class

---
+++ BSPCheck/BSPCheck.cs
@@ -0,0 +1,10 @@
+namespace VSharpBSP.BSPCheck
+{
+    public class BSPCheck
+    {
+        // Write a class to traverse the BSP tree and return the current leaf at the player's position.
+        // Find the current leaf at the player's position.
+        
+        
+    }
+}
